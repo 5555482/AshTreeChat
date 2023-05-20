a268483efd125ea3b013e53de00d638e2d24badf
Nataliia Antonova <antonona@Nataliias-MacBook-Pro.local>
2023-05-20 14:41:04 +0100

    Store and reset message Data

--- backend/functions/database.py
+++ backend/functions/database.py
@@ -1,49 +1,80 @@
-import jsonimport random
+import os
+import json
+import random
 
-# Get recent messages
+# Save messages for retrieval later on
 
 
 def get_recent_messages():
 
-    # Define the file name and learn instruction
+    # Define the file name
     file_name = "stored_data.json"
-    learn_instruction = {
-        "role": "system",
-        "content": "You are interviewing the user for a job as a fullstack developer. Ask short questions that are relevant to the senior position. The user is called Natalia. Keep your answers to under 30 words."
-    }
+    learn_instruction = {"role": "system",
+                         "content": "You are a French teacher and your name is Amelie, the user is called Natalia. Keep responses under 20 words. "}
 
     # Initialize messages
     messages = []
 
-    # Add a random element
+    # Add Random Element
     x = random.uniform(0, 1)
-    if x < 0.5:
+    if x < 0.2:
         learn_instruction["content"] = learn_instruction["content"] + \
-            "Your response will include some dry humour."
+            "Your response will have some light humour. "
+    elif x < 0.5:
+        learn_instruction["content"] = learn_instruction["content"] + \
+            "Your response will include an interesting new fact about Spain. "
     else:
         learn_instruction["content"] = learn_instruction["content"] + \
-            "Your response will include a rather challenging question."
+            "Your response will recommend another word to learn. "
+
+    # Append instruction to message
+    messages.append(learn_instruction)
+
+    # Get last messages
+    try:
+        with open(file_name) as user_file:
+            data = json.load(user_file)
+
+            # Append last 5 rows of data
+            if data:
+                if len(data) < 5:
+                    for item in data:
+                        messages.append(item)
+                else:
+                    for item in data[-5:]:
+                        messages.append(item)
+    except:
+        pass
+
+    # Return messages
+    return messages
 
 
-        # Append instruction to message
-messages.append(learn_instruction)
+# Save messages for retrieval later on
+def store_messages(request_message, response_message):
 
-# Get last messages
+    # Define the file name
+    file_name = "stored_data.json"
 
-try:
-    with open(file_name) as user_file:
-        data = json.load(user_file)
-        if data:
-            if len(data) < 5:
-                for item in data:
-                messages.append(item)
-            else:
-                for item in data[-5:]:
-                    messages.append(item)
+    # Get recent messages
+    messages = get_recent_messages()[1:]
 
-except Exception as e:
-    print(e)
-    pass
+    # Add messages to data
+    user_message = {"role": "user", "content": request_message}
+    assistant_message = {"role": "assistant", "content": response_message}
+    messages.append(user_message)
+    messages.append(assistant_message)
 
-# Return
-return messages
+    # Save the updated file
+    with open(file_name, "w") as f:
+        json.dump(messages, f)
+
+
+# Save messages for retrieval later on
+def reset_messages():
+
+    # Define the file name
+    file_name = "stored_data.json"
+
+    # Write an empty file
+    open(file_name, "w")
