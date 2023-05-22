084ec5e37c211ad45ca8ef33f2bb62cb339150d0
Nataliia Antonova <antonona@Nataliias-MacBook-Pro.local>
2023-05-22 11:25:08 +0100

    Controller component

---
+++ frontend/src/components/Controller.tsx
@@ -0,0 +1,23 @@
+import {useState} from 'react'
+
+function Controller() {
+    const [isLoading, setIsLoading]=useState(false);
+    const [messages,setMessages]=useState<any[]>([]);
+
+const createBlobUrl= (data: any)=>{};
+
+const handleStop = async() =>{};
+
+  return (
+        <div className='h-screen overflow-y-hidden'>
+            <div>
+                Title
+            </div>
+            <div className='flex flex-col justify-between h-full overflow-y-scroll pb-96'>
+                Hi
+            </div>
+        </div>
+  )
+}
+
+export default Controller
