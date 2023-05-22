6c71e3a57d237ef972c80c3a07fe68256fa4c841
Nataliia Antonova <antonona@Nataliias-MacBook-Pro.local>
2023-05-22 13:51:41 +0100

    Controller Recorder Container

--- frontend/src/components/Controller.tsx
+++ frontend/src/components/Controller.tsx
@@ -1,22 +1,27 @@
-import {useState} from 'react'
-import Title from './Title';
+import { useState } from "react";
+import Title from "./Title";
 
 function Controller() {
-    const [isLoading, setIsLoading]=useState(false);
-    const [messages,setMessages]=useState<any[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [messages, setMessages] = useState<any[]>([]);
 
-const createBlobUrl= (data: any)=>{};
+    const createBlobUrl = (data: any) => { };
 
-const handleStop = async() =>{};
+    const handleStop = async () => { };
 
-  return (
-        <div className='h-screen overflow-y-hidden'>
+    return (
+        <div className="h-screen overflow-y-hidden">
             <Title setMessages={setMessages} />
-            <div className='flex flex-col justify-between h-full overflow-y-scroll pb-96'>
-                Hi
+            <div className="flex flex-col justify-between h-full overflow-y-scroll pb-96">
+                {/* Recorder */}
+                <div className="fixed bottom-0 w-full py-6 border-t text-center bg-gradient-to-r from-sky-500 to-green-200">
+                    <div className="flex justify-center items-center w-full">
+                        Recorder
+                    </div>
+                </div>
             </div>
         </div>
-  )
+    );
 }
 
-export default Controller
+export default Controller;
